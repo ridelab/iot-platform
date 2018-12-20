d39402c1c2645c0362cbf8a840eb993d49ccad2e
airt <airtial@gmail.com>
2018-12-20 18:13:11 +0800

    feat: autowire registration listener

--- src/main/java/org/sselab/iot/platform/configuration/LwM2mServerConfiguration.java
+++ src/main/java/org/sselab/iot/platform/configuration/LwM2mServerConfiguration.java
@@ -1,22 +1,27 @@
 package org.sselab.iot.platform.configuration;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.eclipse.leshan.server.LwM2mServer;
 import org.eclipse.leshan.server.californium.LeshanServerBuilder;
+import org.eclipse.leshan.server.registration.RegistrationListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.sselab.iot.platform.configuration.listener.DatabaseRegistrationListener;
 
 @Slf4j
 @Configuration
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class LwM2mServerConfiguration {
+
+  private final RegistrationListener registrationListener;
 
   @Bean
   public LwM2mServer initLwM2mServer() {
     val builder = new LeshanServerBuilder();
     val server = builder.build();
-    server.getRegistrationService().addListener(new DatabaseRegistrationListener());
+    server.getRegistrationService().addListener(registrationListener);
     logger.info("Initialized LwM2mServer {}", server);
     return server;
   }
--- src/main/java/org/sselab/iot/platform/configuration/listener/DatabaseRegistrationListener.java
+++ src/main/java/org/sselab/iot/platform/configuration/listener/DatabaseRegistrationListener.java
@@ -1,14 +1,21 @@
 package org.sselab.iot.platform.configuration.listener;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.leshan.core.observation.Observation;
 import org.eclipse.leshan.server.registration.Registration;
 import org.eclipse.leshan.server.registration.RegistrationListener;
 import org.eclipse.leshan.server.registration.RegistrationUpdate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 
 @Slf4j
+@Primary
+@Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class DatabaseRegistrationListener implements RegistrationListener {
 
   @Override
