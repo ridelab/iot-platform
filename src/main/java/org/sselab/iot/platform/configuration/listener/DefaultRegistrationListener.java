1956a1db9e3742dfd2626dfd0840c752b91007a2
airt <airtial@gmail.com>
2018-12-23 15:57:30 +0800

    feat: lwm2m server registration listener

--- src/main/java/org/sselab/iot/platform/configuration/listener/DefaultRegistrationListener.java
+++ src/main/java/org/sselab/iot/platform/configuration/listener/DefaultRegistrationListener.java
@@ -1,7 +1,11 @@
 package org.sselab.iot.platform.configuration.listener;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.eclipse.leshan.core.observation.Observation;
 import org.eclipse.leshan.server.registration.Registration;
 import org.eclipse.leshan.server.registration.RegistrationListener;
@@ -9,6 +13,8 @@ import org.eclipse.leshan.server.registration.RegistrationUpdate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import org.sselab.iot.platform.domain.Client;
+import org.sselab.iot.platform.repository.ClientRepository;
 
 import java.util.Collection;
 
@@ -18,7 +24,12 @@ import java.util.Collection;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class DefaultRegistrationListener implements RegistrationListener {
 
+  private final ClientRepository clientRepository;
+
+  private final ObjectMapper objectMapper;
+
   @Override
+  @SneakyThrows(JsonProcessingException.class)
   public void registered(
     Registration registration,
     Registration previousRegistration,
@@ -28,7 +39,19 @@ public class DefaultRegistrationListener implements RegistrationListener {
     logger.trace("registration = {}", registration);
     logger.trace("previousRegistration = {}", previousRegistration);
     logger.trace("previousObservations = {}", previousObservations);
+    logger.trace("registration (stringify) = {}", objectMapper.writeValueAsString(registration));
     // TODO
+    val clientOptional = clientRepository.findByRegistrationId(registration.getId());
+    if (clientOptional.isPresent()) {
+      val client = clientOptional.get();
+      logger.trace("client (existed) = {}", client);
+    } else {
+      val client = new Client();
+      client.setRegistrationId(registration.getId());
+      client.setName(registration.getId() + "@" + registration.getEndpoint());
+      clientRepository.save(client);
+      logger.trace("client (new) = {}", client);
+    }
   }
 
   @Override
@@ -55,6 +78,9 @@ public class DefaultRegistrationListener implements RegistrationListener {
     logger.trace("observations = {}", observations);
     logger.trace("newRegistration = {}", newRegistration);
     // TODO
+    val clientOptional = clientRepository.findByRegistrationId(registration.getId());
+    val client = clientOptional.orElseThrow(IllegalStateException::new);
+    logger.trace("client = {}", client);
   }
 
 }
