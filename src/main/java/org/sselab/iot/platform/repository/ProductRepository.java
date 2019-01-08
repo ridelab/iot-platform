e2c87ffadc0d023c6d8865b8b45e39e23379d08e
panteng <xd_panteng@sina.com>
2019-01-08 17:10:09 +0800

    modify

--- src/main/java/org/sselab/iot/platform/repository/ProductRepository.java
+++ src/main/java/org/sselab/iot/platform/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package org.sselab.iot.platform.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.sselab.iot.platform.domain.Product;
 
 import java.util.List;
@@ -14,5 +15,5 @@ import java.util.List;
  */
 public interface ProductRepository extends JpaRepository<Product,Long> {
 
-  List<Product> findByUserId(Long userId);
+  List<Product> findByUserId(@Param("userId") Long userId);
 }
